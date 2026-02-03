da01597fe2a9924d95e925305de7f7d6da991bb6
Teomesa29 <restrepomate@gmail.com>
2026-02-03 13:02:21 -0500

    Update database.py

--- Backend/app/database/database.py
+++ Backend/app/database/database.py
@@ -2,21 +2,19 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-ENV = os.getenv("ENV", "local")
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 if not DATABASE_URL:
-    if ENV == "prod":
-        raise RuntimeError("DATABASE_URL no configurada en producción")
-    DATABASE_URL = "sqlite:///./database/natillera.db"
+    DATABASE_URL = "sqlite:////opt/render/project/src/Backend/app/database/natillera.db"
     connect_args = {"check_same_thread": False}
 else:
-    # Render/Heroku a veces dan postgres://
-    if DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    # fuerza psycopg (psycopg3)
+    if DATABASE_URL.startswith("postgresql://"):
+        DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://", 1)
     connect_args = {}
 
 engine = create_engine(DATABASE_URL, connect_args=connect_args, pool_pre_ping=True)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
@@ -26,3 +24,4 @@ def get_db():
         yield db
     finally:
         db.close()
+
